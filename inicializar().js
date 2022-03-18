4a3e752cbfbc2576af083e435356761277f50422
nachozg <ig.zugarramurdi@gmail.com>
2022-03-18 12:08:48 -0300

    ApellidoNombre

--- inicializar().js
+++ inicializar().js
@@ -1,5 +1,67 @@
-inicializar()
-function repaso (){
-    let repaso = olvido
-    const memoryLost = permanent
+// clase 2 entrega 1 
+let usuarios = [];
+let mascotas = [];
+let libros = [];
+
+class Usuario {
+    constructor(pNombre, pApellido, pLibros, pMascotas) {
+        this.nombre = pNombre;
+        this.apellido = pApellido;
+        this.libros = pLibros;
+        this.mascotas = addMascotas(pMascotas);
+        this.nombreCompleto = getFullName(pNombre, pApellido);
+        this.cantidadMascotas = countMascotas(pMascotas);
+        
+    }
+    getFullName(nombre, apellido) {
+        let nombreCompleto = `${nombre} ${apellido}`;
+        return nombreCompleto
+    }
+    addMascotas(mascota) {
+        let mascotasUsuario = [];
+        for (let i = 0; i < mascota.length; i++) {
+            let mascotaActual = mascota[i];
+            mascotasUsuario.push(mascotaActual);
+
+        }
+        return mascotasUsuario
+
+
+    }
+    countMascotas(mascotas) {
+        let contadorMascotas = 0;
+        for (let i = 0; i < mascotas.length; i++) {
+            let mascotaActual = mascotas[i];
+            contadorMascotas++;
+
+        }
+        return contadorMascotas
+    }
+
+
+    addBooks(libro, autor) {
+        nuevoLibro = `titulo: ${libro} autor: ${autor}`
+        libros.push(nuevoLibro);
+    }
+    getBookNames(libros){
+        let titulosUsuario = [];
+        for (let i = 0; i < libros.length; i++) {
+            let libroActual = libros[i];
+            let tituloLibro = libroActual.nombreLibro;
+            titulosUsuario.push(tituloLibro);
+            
+        }
+        return titulosUsuario
+
+    }
 }
+
+function subirUsuario(nombre, apellido, libros, mascotas) {
+    let usuario = new Usuario(nombre, apellido, libros, mascotas);
+    usuarios.push(usuario)
+
+
+}
+librosGonzalo = ['2 vueltas en 4','aventuras en el regency']
+mascotasGonzalo = ['gallina','dawson']
+subirUsuario(Gonzalo, Bergessio, librosGonzalo, mascotasGonzalo)
