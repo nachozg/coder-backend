5a9da33c17db73eb4e95b4714fedf0f295eb81c4
nachozg <ig.zugarramurdi@gmail.com>
2022-04-05 10:15:11 -0300

    cambios servidor

--- desafio3.js
+++ desafio3.js
@@ -1,137 +1,146 @@
-const express = require('express');
-const moment = require('moment');
-const app = express();
-const Contenedor = require('./desafio3')
-
-const nuevoProducto = new Contenedor("./productos.txt")
-const PORT = process.env.PORT || 8080
-
-const server = app.listen(PORT,()=>{
-    console.log(`Server on port ${PORT} `);
-})
-
-server.on("error",(err)=>{
-    console.log(`Error: ${err.message}`);
-})
+const fs = require('fs').promises;
 
 
-// R U T A S
-
-
-app.get('/productos',(req,res)=>{
-    nuevoProducto.getAll()
-    .then(data =>{
-        res.json(data);
-    })
-    .catch(err =>{
-        res.send(err)
-    })
-})
-
-app.get('/productoRandom',(req,res)=>{
-    nuevoProducto.getProductRandom()
-    .then(productRandom =>{
-        res.json(productRandom);
-
-    })
-    .catch(err =>{
-        res.send(err)
-    })
-})
-// modulos
-
-import {
-    promises as fs
-} from 'fs';
-let proximoId = 0;
 
 class Contenedor {
-    constructor(pArchivo) {
-
-        this.archivo = pArchivo
-        this.arrayProductos = []
-
+    constructor(fileName) {
+       
+        this.fileName = fileName;
+        this.products = [];
     }
-    async save(objeto) {
-        try {
-            let archivoActual = await fs.readFile(this.archivo, "utf8");
-            let archivoAObjeto = JSON.parse(archivoActual);
-            //agregar id al objeto nuevo que se va a guardar
-            objeto.id = proximoId + 1;
-            archivoAObjeto.push(objeto);
-            proximoId++;
 
-            let volverArchivoAChar = JSON.stringify(archivoAObjeto);
-            await fs.writeFile(this.archivo, volverArchivoAChar);
-            return proximoId;
+        async fileJson(){ 
+       let file =  await fs.readFile(this.fileName,'utf8');
+        let json = JSON.parse(file);
+        return json
+       
+       
+    }   
+       
+
+    async saveFile(params){
+        let jsonString = JSON.stringify(params,null,2);
+        await fs.writeFile(this.fileName, jsonString);
+    }
+   
+    
 
 
-        } catch (error) {
-            console.log(error) //
+
+    async save(newObj){
+        // .Recibe un objeto, lo guarda en el archivo productos, devuelve el id asignado.
+         
+        try{
+            
+
+            let file = await fs.readFile(this.fileName, "utf8");
+            
+           //Si no hay datos crea el id
+            if(file === ''){
+                newObj.id = 1
+                
+               this.products.push(newObj);
+            }else{
+            //Si lo hay lo suma al anterior
+           const json = JSON.parse(file)
+           newObj.id = json[json.length-1].id +1;     
+           json.push(newObj);      
+           this.products = json;
+           this.saveFile(json)
+        }
+        
+        console.log('Producto guardado de forma correcta')
+
+        return newObj.id
+    }
+    
+
+      
+        catch(error){
+            console.log(error);
         }
 
     }
-    async getById(id) {
 
-        try {
-            let leerArchivo = await fs.readFile(this.archivo, 'utf8');
-            let json = JSON.parse(leerArchivo);
-            let productoXID = json.find(productoXID => productoXID.id == id);
-            return console.log(productoXID);
-        } catch {
-            return null
-        }
-    }
-    async getAll() {
-        //Devuelve un array con todos los objetos del archivo
-        try {
-            let file = await fs.readFile(this.archivo, 'utf8');
-            let json = JSON.parse(file)
-            this.products = json;
-            return console.log(this.products)
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    async deleteById(id) {
-        //Elimina un objeto mediante su ID
-        try {
-            let leerArchivo = await fs.readFile(this.archivo, 'utf8');
-            let json = JSON.parse(leerArchivo)
-            let product = json.filter(product => product.id != id);
-            this.arrayProductos = product;
-            let jsonString = JSON.stringify(product);
-            fs.writeFile(this.archivo, jsonString);
-        } catch (err) {
-            console.log(err)
-        }
-    }
-    async deleteAll() {
-        //Elimina todos los objetos del archivo
-        try {
-            let productos = [];
-            this.arrayProductos = productos;
-            let jsonString = JSON.stringify(productos);
-            fs.writeFile(this.archivo, jsonString);
-        } catch (err) {
-            console.log(err)
-        }
-    }
-
-// toma un producto random y lo devuelve
-    async getProductRandom() {
+    async getProductRandom(){
         //Devuelve un objeto al amazonArticle
 
 
-        try {
+        try{
             let json = await this.fileJson()
 
-            let random = Math.floor(Math.random() * (json.length));
+            let random = Math.floor(Math.random() *(json.length));
             return json[random];
-        } catch (err) {
+        }
+        catch(err){
             console.log(err)
         }
     }
+   
+
+  async getById(id){
+
+    //Recibe un id y devuelve el objeto con ese id o null si no existe
+      try{
+       
+    
+       let json = await this.fileJson()
+      
 
 
+         
+          let product = json.find(product => product.id == id);
+          return console.log(product);
+      }
+      catch(err){
+         console.log('No funciona')
+      }
+  }
+  async getAll(){
+
+     //FORMA 2
+      //Devuelve un array con todos los objetos del archivo
+      try{
+   let json = await this.fileJson()
+     this.products = json
+          return this.products
+      
+        
+      }
+      catch(err){
+          console.log(err)
+      }
+  }
+
+  async deleteById(numero){
+    //Elimina un objeto mediante su ID
+    try{
+        let json = await this.fileJson()
+
+      let product=json.find(product =>product.id === numero );
+      const index = json.indexOf(product);
+      json.splice(index,1);
+    
+      
+    this.saveFile(json)
+    }
+    catch(err){
+        console.log(err)
+    }
 }
+async deleteAll(){
+    //Elimina todos los objetos del archivo
+    try{
+        let product=[];
+        this.products = product;
+        this.saveFile(product)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+}
+
+
+
+module.exports =Contenedor;
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const moment = require('moment');
+const app = express();
+const Contenedor = require('./challenge3')
+
+const nuevoProducto = new Contenedor("./products.txt")
+const PORT = process.env.PORT || 8080
+
+const server = app.listen(PORT,()=>{
+    console.log(`Server on port ${PORT} `);
+})
+
+server.on("error",(err)=>{
+    console.log(`Error: ${err.message}`);
+})
+
+
+// R U T A S
+
+
+app.get('/productos',(req,res)=>{
+    nuevoProducto.getAll()
+    .then(data =>{
+        res.json(data);
+    })
+    .catch(err =>{
+        res.send(err)
+    })
+})
+
+app.get('/productoRandom',(req,res)=>{
+    nuevoProducto.getProductRandom()
+    .then(productRandom =>{
+        res.json(productRandom);
+
+    })
+    .catch(err =>{
+        res.send(err)
+    })
+})
